97b8ceb0396210cea89cda8b36bc915bc4d7d578
Md.Rakib hossen <rlorakib2002@gmail.com>
2023-03-05 14:28:01 +0600

    update data

--- todo/update.php
+++ todo/update.php
@@ -1,6 +1,43 @@
 <?php
 include "database.php";
-?>
+?> 
+
+<?php
+ 
+$id = $_GET['update'];
+ echo "Your old data -id:".$id;   
+
+$sql = "SELECT task FROM Todo WHERE id=$id ";
+$result = mysqli_query($con,$sql);
+
+if(mysqli_num_rows($result)> 0){
+    while($row =mysqli_fetch_assoc($result)){
+       echo " task:".$row['task'];
+
+    }
+}
+if(isset($_REQUEST['submit'])){
+    $new= $_REQUEST["submit"];
+    echo $new;
+    
+}else{
+    echo null;
+}
+if(isset($new)){
+    if(empty($new)){
+        echo '<p style="color:red">Sorry please enter new text here</p>';
+    }
+}
+
+if(!empty($new)){
+    $task = "UPDATE Todo SET task='$new' WHERE id=$id ";
+    mysqli_query($con, $task);
+    header("location:insert.php");
+}
+
+
+?> 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,33 +47,10 @@ include "database.php";
     <title>Update</title>
 </head>
 <body>
-    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-        Task: <input type="text" name="task" placeholder="new data submit" >
+    <form action="" method="post">
+        <input type="text" name="submit" placeholder="Enter new text here....">
         <input type="submit" value="submit">
-        
-        
     </form>
-   <?php
-   $erro='';
-$id = $_GET['update'];
-    
-
-if($_SERVER['REQUEST_METHOD']=='POST'){
-    $new= $_POST["task"];
-    
-}else{
-    echo "Error";
-}
-if(isset($new)){
-    if(empty($new)){
-        echo '<p style="color:red">Sorry please enter new text here</p>';
-    }else{
-        $data = $new ;
-        $task = "UPDATE Todo SET task=$data WHERE id=$id";
-        mysqli_query($con, $task);
-        header("location:insert.php");
-    }
-}
-?> 
+ 
 </body>
 </html>
